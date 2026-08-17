3ef456108c80ffab89f7eeba50ca083885e4818f
LU <3227839919@qq.com>
2026-08-17 16:40:06 +0800

    fix：补充伤力回天缺少对应的头文件

--- GuZhenRenPersonalCode/Cards/LiDao/ShaZhao/ShangLiHuiTian.cs
+++ GuZhenRenPersonalCode/Cards/LiDao/ShaZhao/ShangLiHuiTian.cs
@@ -12,6 +12,7 @@ using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
 
 namespace GuZhenRen.Cards.ShaZhao;
 
